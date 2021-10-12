25a828b4b4d74f294bd234d9d2d6709d19a2e64b
Óscar A. Montiel <etherealflux94@gmail.com>
2021-10-12 12:22:56 -0500

    Turns on Last.fm

--- animanoir.js
+++ animanoir.js
@@ -19,17 +19,17 @@ import {
 
 //TODO Turn on Last.fm fetching before pushing
 
-// const lastfmData = fetch('https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=swoephowx&api_key=8d1394415d95c0771ac9f8247cc7ee17&limit=1&nowplaying=true&format=json')
-//   .then(
-//     response => response.json()
-//   )
-//   .then(data => {
-//     // Removes quotes from JSON data
-//     const formattedTrackname = JSON.stringify(data.recenttracks.track[0].name).replace(/["]+/g, '')
-//     const formattedArtistname = JSON.stringify(data.recenttracks.track[0].artist['#text']).replace(/["]+/g, '')
-//     document.getElementById("track").textContent = formattedTrackname
-//     document.getElementById("artist").textContent = formattedArtistname
-//   });
+const lastfmData = fetch('https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=swoephowx&api_key=8d1394415d95c0771ac9f8247cc7ee17&limit=1&nowplaying=true&format=json')
+  .then(
+    response => response.json()
+  )
+  .then(data => {
+    // Removes quotes from JSON data
+    const formattedTrackname = JSON.stringify(data.recenttracks.track[0].name).replace(/["]+/g, '')
+    const formattedArtistname = JSON.stringify(data.recenttracks.track[0].artist['#text']).replace(/["]+/g, '')
+    document.getElementById("track").textContent = formattedTrackname
+    document.getElementById("artist").textContent = formattedArtistname
+  });
 
 /* -------------------------------- three.js -------------------------------- */
 
